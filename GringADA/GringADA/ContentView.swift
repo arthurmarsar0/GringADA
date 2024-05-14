0b8526f7287ff413f6ab9129ff324b1a2b633f69
Arthur Marsaro <aam4@cin.ufpe.br>
2024-05-14 15:16:17 -0300

    Adiciona o Projeto Base + ColorGuide no Xcode

---
+++ GringADA/GringADA/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  GringADA
+//
+//  Created by Arthur Alves Marsaro on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GringADA/GringADA/GringADAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GringADAApp.swift
+//  GringADA
+//
+//  Created by Arthur Alves Marsaro on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GringADAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
