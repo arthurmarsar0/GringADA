a49c307883ea55464f50296683be4f7514afb2e7
Arthur Marsaro <aam4@cin.ufpe.br>
2024-05-16 17:36:34 -0300

    Adiciona fonte do texto, restante das perguntas, botão de finalizar (ainda sem funcionalidade)

--- GringADA/GringADA/ContentView.swift
+++ GringADA/GringADA/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     @State var answer2: Int?
     @State var answer3: Int?
     @State var answer4: Int?
-    @State var answer5$: Int?
+    @State var answer5: Int?
     
     var body: some View {
         ZStack {
@@ -26,11 +26,16 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             //MiddleGround - onde vai ficar os componentes que serão scrolaveis
             VStack(spacing: 0.0) {
-                Rectangle()  // lembrar de adicionar o titulo do aplicativo ... 
-                    .foregroundColor(.color4)
-                    .ignoresSafeArea()
-                    .frame(width: 393)
-                    .frame(maxHeight: 70.0)
+                ZStack(alignment: /*@START_MENU_TOKEN@*/Alignment(horizontal: .center, vertical: .center)/*@END_MENU_TOKEN@*/){
+                    Rectangle()  // lembrar de adicionar o titulo do aplicativo ...
+                        .foregroundColor(.color4)
+                        .ignoresSafeArea()
+                        .frame(width: 393)
+                    .frame(maxHeight: 80.0)
+                    Text("gringADA")
+                        .foregroundStyle(.color1)
+                        .font(Font.custom("LeagueGothic-Regular", size:56.0))
+                }
                 ScrollView {
                     VStack(alignment: .leading, spacing: 24.0){
                         Text("Qual a sua compatibilidade com alguns idiomas?")
@@ -64,14 +69,28 @@ struct ContentView: View {
                         Spacer()
                         VStack(alignment: .leading, spacing: 60.0) {
                             
-                            Divider()
+                            //Divider()
                             QuestionView(selectedItem: $answer1, questionText: "Você é uma pessoa que se comunica com muito toques/gestos")
                             Divider()
                             QuestionView(selectedItem: $answer2, questionText: "Normalmente um me comunico diretamente se eu discordo ou concordo de algo")
                             Divider()
-                            QuestionView(selectedItem: $answer3, questionText: "Tacotaracotaracotaco sua piroka em meu tabaco vai batendo no meu rim")
-                            Spacer()
-                            // Button
+                            QuestionView(selectedItem: $answer3, questionText: "Prefiro obras literárias com predominancia de texto à imagens")
+                            Divider()
+                            QuestionView(selectedItem: $answer4, questionText: "Em um idioma, eu me fascino mais pela relação entra as palavras do que a singularidade de cada palavra em si")
+                            Divider()
+                            QuestionView(selectedItem: $answer5, questionText: "Tenho costume de consumir conteúdo de outras culturas em seu idioma de origem (e.g: Musicas no idioma original e filmes/séries legendada)")
+                            Divider()
+                            Button(action: {}, label: {
+                                ZStack{
+                                    Color.color3
+                                    Text("FINISH")
+                                        .foregroundStyle(.color1)
+                                    //  .font(.___)
+                                }
+                                .cornerRadius(24.0)
+                            })
+                            .frame(width: 164, height: 36.0)
+                            .frame(maxWidth: .infinity)
                             
                         }
                         
--- GringADA/GringADA/StyleGuide.swift
+++ GringADA/GringADA/StyleGuide.swift
@@ -11,5 +11,5 @@ extension Font {
     static let header1: Font = .system(size:24, weight: .bold, design: .rounded)
     static let header2: Font = .system(size:16, design: .rounded)
     static let Questions: Font = .system(size:20, design: .rounded)
-    //static let Title: Font = .system(size:  , weight: .bold, design: .rounded)
+    static let Title: Font = .system(size:  56, weight: .bold, design: .rounded)
 }
